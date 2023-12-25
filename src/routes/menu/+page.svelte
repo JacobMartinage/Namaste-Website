7ffb377b1464f1074603c1e8012b0014397af99d
ramankc6 <ramankc699@gmail.com>
2023-12-25 08:40:11 -0500

    uploading locally hosted website

---
+++ src/routes/menu/+page.svelte
@@ -0,0 +1,14 @@
+<style>
+    img {
+        width: 100vw;
+        height: auto;
+    }
+</style>
+
+<img src = 'Hamro Kitchen Menu-1.png' alt = 'page 1'>
+<img src = 'Hamro Kitchen Menu-2.png' alt = 'page 2'>
+<img src = 'Hamro Kitchen Menu-3.png' alt = 'page 3'>
+<img src = 'Hamro Kitchen Menu-4.png' alt = 'page 4'>
+<img src = 'Hamro Kitchen Menu-5.png' alt = 'page 5'>
+<img src = 'Hamro Kitchen Menu-6.png' alt = 'page 6'>
+<img src = 'Hamro Kitchen Menu-7.png' alt = 'page 7'>
